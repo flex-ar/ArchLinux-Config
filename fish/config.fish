67b8c138d8f6f36a184c180de21819332d98a293
flex-ar <jmarnedo8@gmail.com>
2023-03-15 11:48:53 -0300

    update config fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+  starship init fish | source
 end
 
 # Aliases
-
 alias ls "exa --icons -h -F"
 alias la "exa --icons -l -a -h -F"
 alias cat "bat -p"
 alias vim "nvim"
+alias hora "tty-clock -f %d/%m/%y -c"
 
-starship init fish | source
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/flex/.ghcup/bin # ghcup-env
---
+++ fish/functions/gum-choose.fish
@@ -0,0 +1,3 @@
+function gum-choose
+  gum choose --cursor "  " --cursor.bold --cursor.foreground "005" $argv
+end
